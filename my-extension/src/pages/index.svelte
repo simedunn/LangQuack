72e546838bbfcd74b2a904a41c7211a92f4bd36c
Dunn, Simeon <simedunn01@gmail.com>
2023-04-16 10:44:01 -0400

    packages

--- my-extension/src/pages/index.svelte
+++ my-extension/src/pages/index.svelte
@@ -1,6 +1,5 @@
 <script>
-  import Langquack from '../components/langquack.svelte';
-import LangQuack from '../components/langquack.svelte'
+  import LangQuack from '../components/langquack.svelte';
 </script>
 
 <div>
